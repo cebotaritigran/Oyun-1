5a80ba76ec00fd5b8ce1e30e01a80407a00e246e
Ercan Tomaç <ercan2243ercan@gmail.com>
2024-02-13 23:35:51 +0100

    merağın yok mu böyle şeylere?

--- Assets/Code/Scripts/PrefabGenerator.cs
+++ Assets/Code/Scripts/PrefabGenerator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using System.Threading.Tasks;
+using System.Linq;
+using UnityEngine.UI;
 
 public class Instantiation : MonoBehaviour
 {
@@ -10,20 +12,15 @@ public class Instantiation : MonoBehaviour
 
     void Awake() => GlobalInstance = this;
 
-    private int firstCard = -1;
-    private int secondCard = -1;
-    private const int gridRows = 4;
-    private const int gridCols = 8;
-    //private const float offsetX = 100f;
-    //private const float offsetY = 100f;
-
-
+    private int _firstCardIndex = -1;
+    private int _secondCardIndex = -1;
+    private const int _gridRows = 4;
+    private const int _gridCols = 8;
+    private const int _dimension = _gridRows * _gridCols;
     private const int _screenWidth = 1920;
     private const int _screenHeight = 1080;
-
-    private const float _cardWidth = _screenWidth / gridCols;
-    private const float _cardHeight = _screenHeight / gridRows;
-
+    private const float _cardWidth = _screenWidth / _gridCols;
+    private const float _cardHeight = _screenHeight / _gridRows;
     private const float _mostRight = (_screenWidth / 2) - (_cardWidth / 2);
     private const float _mostLeft = _mostRight * -1;
     private const float _topMost = (_screenHeight / 2) - (_cardHeight / 2);
@@ -31,18 +28,20 @@ public class Instantiation : MonoBehaviour
 
     [SerializeField] private Card myPrefab;
 
+    private List<int> _randomNumbers;
+    private List<bool> _isMatched = Enumerable.Range(0, _dimension).Select(x => false).ToList();
+
     private List<Card> _cards = new List<Card>();
     private List<Animator> _animators = new List<Animator>();
 
     private void Start()
     {
-        //GameObject box = GameObject.Find(/* "Box" */"Cube");
-        //int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
-        for (int row = 0; row < gridRows; row++)
+        _initializeGameState();
+        for (int row = 0; row < _gridRows; row++)
         {
-            for (int col = 0; col < gridCols; col++)
+            for (int col = 0; col < _gridCols; col++)
             {
-                int index = (gridCols * row) + col;
+                int index = (_gridCols * row) + col;
                 float xCoordinate = _mostLeft + (col * _cardWidth);
                 float yCoordinate = _topMost - (row * _cardHeight);
 
@@ -53,20 +52,61 @@ public class Instantiation : MonoBehaviour
                 //spawnedCard.transform.position = new Vector3(xCoordinate, yCoordinate, 0);
                 //spawnedCard.transform.rotation = Quaternion.identity;
 
-
-                spawnedCard.transform.localScale = new Vector3(_cardWidth - 20, _cardHeight - 20, 1);
+                spawnedCard.transform.localScale = new Vector3(_cardWidth - 20, _cardHeight - 20, 10);
                 spawnedCard.index = index;
+
+                //Text textComponent = spawnedCard.GetComponent<Text>();
+                //textComponent.text = _randomNumbers[index].ToString();
+                
                 _cards.Add(spawnedCard);
                 _animators.Add(animator);
 
-                staggeredAnimation();
+                _staggeredAnimation();
             }
         }
     }
 
-    private async void staggeredAnimation(){
+    private void _initializeGameState()
+    {
+        _randomNumbers = _generateRandomNumbers(_dimension / 2);
+
+        List<int> indices = Enumerable.Range(0, _dimension).ToList();
+
+        _randomNumbers.AddRange(_randomNumbers.GetRange(0, _randomNumbers.Count));
+        _randomNumbers.Shuffle();
+
+        List<int> tmpRandomNumbers = new List<int>(_randomNumbers);
+        _randomNumbers.Clear();
+
+        for (int j = 0; j < _dimension; j++)
+        {
+            _randomNumbers.Add(tmpRandomNumbers[indices[j]]);
+        }
+    }
+
+    private List<int> _generateRandomNumbers(int dimension)
+    {
+        List<int> randomNumbers = new List<int>();
+
+        for (int i = 0; i < dimension; i++)
+        {
+            System.Random rnd = new System.Random();
+            int randomNumber = rnd.Next();
+            while (randomNumbers.Contains(randomNumber))
+            {
+                randomNumber = rnd.Next();
+            }
+            randomNumbers.Add(randomNumber);
+        }
+
+        return randomNumbers;
+    }
+
+    private async void _staggeredAnimation()
+    {
         await Task.Delay(1000);
-        for(int i = 0; i < _cards.Count; i++){
+        for (int i = 0; i < _cards.Count; i++)
+        {
             await Task.Delay(100);
             _animators[i].Play("Base Layer.OpenCard", 0, 0.0f);
         }
@@ -75,14 +115,14 @@ public class Instantiation : MonoBehaviour
     public void handleCardClick(int index)
     {
         _animators[index].Play("Base Layer.OpenCard", 0, 0.0f);
-        if (firstCard == -1)
+        if (_firstCardIndex == -1)
         {
-            firstCard = index;
+            _firstCardIndex = index;
             _openCard(index);
         }
-        else if (secondCard == -1)
+        else if (_secondCardIndex == -1)
         {
-            secondCard = index;
+            _secondCardIndex = index;
             _openCard(index);
             _checkMatch();
         }
@@ -92,6 +132,16 @@ public class Instantiation : MonoBehaviour
     {
         //Debug.Log(firstCard);
         //Debug.Log(secondCard);
+        if (_randomNumbers[_firstCardIndex] == _randomNumbers[_secondCardIndex])
+        {
+            // CORRECT MATCH
+            Debug.Log("CORRECT MATCH");
+        }
+        else
+        {
+            // FALSE MATCH
+            Debug.Log("FALSE MATCH");
+        }
     }
 
     private void _openCard(int index)
@@ -102,5 +152,22 @@ public class Instantiation : MonoBehaviour
     private void _closeCard(int index)
     {
         _cards[index].transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+}
+
+static class MyExtensions
+{
+    private static System.Random rng = new System.Random();
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
     }
 }
